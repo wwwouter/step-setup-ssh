297c636252afb487874ac3deb1df9d3ac085f5f7
wwwouter <wouter@wercker.com>
2013-08-28 15:14:29 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,6 +5,16 @@ echo -e "$WERCKER_SETUP_SSH_PRIVATE_KEY" > "$WERCKER_SETUP_SSH_ID_FILE"
 #set permission
 chmod 0700 "$WERCKER_SETUP_SSH_ID_FILE"
 
-export WERCKER_SSH="ssh $WERCKER_SETUP_SSH_HOST -v -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -l $WERCKER_SETUP_SSH_USERNAME -i $WERCKER_SETUP_SSH_ID_FILE"
+export WERCKER_SSH="ssh $WERCKER_SETUP_SSH_HOST -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -l $WERCKER_SETUP_SSH_USERNAME -i $WERCKER_SETUP_SSH_ID_FILE"
+
+export result="$($WECKER_SSH echo 1)"
+exitCode=$?
+
+if [ $exitCode != "0" ] ; then
+  error "Could not connect"
+  $WERCKER_SSH -v echo 1
+  exit 1
+fi
+  
 
 success "Created WERCKER_SHH $WERCKER_SSH"
