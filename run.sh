8cb2c9b844227c777da85ade1347b8da3cf3e6f6
wwwouter <wouter@wercker.com>
2013-09-17 10:22:55 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ echo -e "$WERCKER_SETUP_SSH_PRIVATE_KEY" > "$WERCKER_SETUP_SSH_ID_FILE"
 #set permission
 chmod 0700 "$WERCKER_SETUP_SSH_ID_FILE"
 
-export WERCKER_SSH="ssh $WERCKER_SETUP_SSH_HOST -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -l $WERCKER_SETUP_SSH_USERNAME -i $WERCKER_SETUP_SSH_ID_FILE"
+export WERCKER_SSH="ssh $WERCKER_SETUP_SSH_HOST -t -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -l $WERCKER_SETUP_SSH_USERNAME -i $WERCKER_SETUP_SSH_ID_FILE"
 
 $WERCKER_SSH echo 1 &> /dev/null || failed=true
 
